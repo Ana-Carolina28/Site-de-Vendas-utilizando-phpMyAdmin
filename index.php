3d44cd7748e040df2eb4d85caf20c76162a6c528
Ana Carolina Vargas <71799706+Ana-Carolina28@users.noreply.github.com>
2022-09-02 16:51:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="icon" href="img/icon.png">
+</head>  
+
+<body>
+    <nav id="menu-h">
+        <ul>
+            <a href="http://127.0.0.1/biju/index.php"><img class="logo" src="img/logo.png"></a>
+            <li><a href="http://127.0.0.1/biju/cadastro.php">Cadastro</a></li>
+            <li><a href="http://127.0.0.1/biju/buscar.php">Buscar</a></li>
+            <li><a href="#brincos">Brincos</a></li>
+            <li> <a href="#aneis">Anéis</a></li>
+            <li><a href="#colares">Colares</a></li>
+            <li> <a href="#pulseiras">Pulseiras</a></li>
+        </ul>
+    </nav>
+
+    <div class="container-inicio">
+        <img src="img/joia.png">
+    </div>
+    <br>
+
+    <div id="aneis" class="produto">
+        <span class="prod">
+            
+            <img src="img/anelazulclaro.jpg" alt="">
+            <h3>Bella - Turmalina Paraíba</h3>
+        
+               <p> R$ 185,00</p>
+
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+            
+            <img src="img/anelazul.jpg" alt="">
+            <h3>Anel Diamond</h3>
+            <p>
+                R$ 240,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+            <img src="img/alianca.jpg" alt="">
+            <h3>Zirconia Noivado</h3>
+            <p>
+                R$ 124,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+    </div>
+    <br>
+    <div id="colares" class="produto">
+        <span class="prod">
+            <img src="img/pulseira-coracao.png" alt="">
+            <h3>Colar em Banho de Ouro Amarelo</h3>
+
+            <p>
+                R$ 460,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+            
+            <img src="img/espinelio.png" alt="">
+            <h3>Colar Ouro Espinélio</h3>
+            <p>
+                R$ 89,90
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+           
+            <img src="img/opala.png" alt="">
+            <h3>Colar em Ouro Rosé </h3>
+            <p>
+                R$ 175,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+    </div>
+    <br>
+    <div id="pulseiras" class="produto">
+        <span class="prod">
+            <img src="img/pulseira1.png" alt="">
+            <h3>Pulseira Banho de Ouro</h3>
+
+            <p>
+                R$ 1.490,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+            <img src="img/pulseira2.png" alt="">
+            <h3>Pulseira Grace</h3>
+
+            <p>
+                R$ 220,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+          
+            <img src="img/pulseira3.jpg" alt="">
+            <h3>Pulseira Envolver 18k</h3>
+            <p>
+                R$ 22.990,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+
+       <br>
+    </div>
+
+    <div class="banner">
+        <img src="img/banner2.png">    
+    </div>
+    
+    <div id="brincos" class="produto">
+        <span class="prod">
+            
+            <img src="img/brinco1.png" alt="">
+            <h3>Brinco Relevo em Ouro</h3>
+            <p>
+                R$ 990,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+            <img src="img/brinco2.png" alt="">
+            <h3>Brinco Argola Estrela</h3>
+
+            <p>
+                R$ 89,99
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+        <span class="prod">
+           
+            <img src="img/brinco3.png" alt="">
+            <h3>Brinco Irradiar Ouro</h3>
+            <p>
+                R$ 8.990,00
+            </p>
+            <button  class="button button1" onclick="alert('Adicionado ao Carrinho!')">Comprar</button>
+
+        </span>
+    </div>
+<br><br>
+    <footer>
+        Copyright © 2021, todos os direitos reservados.
+    </footer>
+</body>
+</html>
